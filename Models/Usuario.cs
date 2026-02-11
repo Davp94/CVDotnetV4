9c29c9c3e8ee730533c2c1144e4b13123871f901
david <david.villca.pacheco@gmail.com>
2026-02-10 22:56:49 -0400

    add pagination sorting filtering

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -17,4 +17,6 @@ public class Usuario
     public ICollection<Rol> Roles { get; set; } = [];
 
     public ICollection<RefreshToken> RefreshTokens { get; set; } = [];
+
+    public ICollection<SucursalUser> SucursalUsers { get; set; } = [];
 }
---
+++ Services/spec/IProductoService.cs
@@ -0,0 +1,13 @@
+using System;
+using ComprasVentas.Dto;
+using ComprasVentas.Dto.common;
+
+namespace ComprasVentas.Services.spec;
+
+public interface IProductoService
+{
+
+    Task<PageResultDto<ProductoDto>> GetProductosAsync(ProductoFilterDto productoFilterDto);
+
+    Task<ProductoDto> CreateAsync(CreateProductoDto createProductoDto);
+}
